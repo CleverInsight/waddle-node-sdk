9418893080a1604b8e75e0eaee0e72bb216be36c
Bastin Robins J <bastinrobins@gmail.com>
2023-01-03 16:41:02 +0530

    Modified: Basic README documentation

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # waddle-node-sdk
-Waddle Node SDK 
+Waddle Node SDK
+
+Access Waddle SDK to perform all `CREATE`, `UPDATE`, `DELETE` and `SELECT` bucket and metrics
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+
+
+class waddle {
+  auth_key: string
+  auth_token: string
+  bearer: string
+  
+  constructor(auth_key:string, auth_token:string) {
+  	this.auth_key = auth_key;
+	this.auth_token = auth_token;
+
+	// Get the access token here
+  }
+	
+  // getBucket method to fetch `id` matching bucket
+  
+
+}
